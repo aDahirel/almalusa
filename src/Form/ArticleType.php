8e5783600e5fc07c1f16156dc14e7fcb217a1842
aDahirel <axel.dahirel@gmail.com>
2020-03-28 12:01:29 +0100

    Image upload feature
    _ Added the "vich/uploader-bundle"
    _ Removed the image attribut in Article Entity
    _ Added updated_at, fileName and Imagefile in Article Entity
    _ ArticleType modified for the new image system
    _ Added show and edit methods for the image

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -8,19 +8,24 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class)
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'title'
             ])
             ->add('content')
-            ->add('image')
+            ->add('imageFile', FileType::class, [
+                'required' => false
+            ])
         ;
     }
 
