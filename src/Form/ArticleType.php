a2e75fecff15d440e60deb512cd0b85008b2483b
jeanlagniaux <34351725+jeanlagniaux@users.noreply.github.com>
2020-04-01 12:07:06 +0200

    handle too big file on create

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -12,6 +12,8 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 
 class ArticleType extends AbstractType
@@ -28,16 +30,11 @@ class ArticleType extends AbstractType
             ->add('content')
             ->add('imageFile', FileType::class, [
                 'required' => false,
-                'mapped' => false,
                 'constraints' => [
-                    new File([
-                        'maxSize' => '10k',
-                        'mimeTypes' => [
-                            "image/jpeg", "image/png", "image/jpg", "image/gif",
-                        ],
-                        'mimeTypesMessage' => 'Please upload a valid image document',
-                    ])
-                ]
+                    new Image([
+                        'maxSize' => '4M',
+                    ]),
+                ],
             ]);
     }
 
@@ -48,3 +45,16 @@ class ArticleType extends AbstractType
         ]);
     }
 }
+
+/*
+ *
+                'constraints' => [
+                    new File([
+                        'maxSize' => '10k',
+                        'mimeTypes' => [
+                            "image/jpeg", "image/png", "image/jpg", "image/gif",
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image document',
+                    ])
+                ]
+ */
