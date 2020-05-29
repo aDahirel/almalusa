de4861f010867e33e06c4d58fe850d54fa91f33f
aDahirel <axel.dahirel@gmail.com>
2020-05-29 20:51:57 +0200

    membership update and some  javascripts

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,6 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css'
+import Filter from '../../assets/js/modules/Filter'
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+new Filter(document.querySelector('.js-filter'))
