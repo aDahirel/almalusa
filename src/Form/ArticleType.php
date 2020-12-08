4357e0686bac93ab03b141779f4332b479b641d2
Woodyyy <36766352+Woodyyy@users.noreply.github.com>
2020-12-08 23:55:28 +0100

    Revert "Revert "First commit / Project update""
    This reverts commit 7affefb8f26f9fec2735d119a4f1e9c812d6ee75.

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\Image;
+
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title',
+                'multiple' => true
+            ])
+            ->add('content', CKEditorType::class, [
+                'required' => false,
+                'config' => [
+                    'uiColor' => "#e2e2e2",
+                    'toolbar' => 'standard'
+                ]
+            ])
+            ->add('imageFile', FileType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Image([
+                        'maxSize' => '4M',
+                        'mimeTypes' => 'image/*',
+                        'mimeTypesMessage' => 'Le fichier ne correspond pas à une photo.'
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
+
